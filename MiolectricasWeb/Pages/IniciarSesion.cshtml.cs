948994069d974c11d0f991e68ad0b43bcba5cada
Jose Daniel Ramirez Bastidas <danik_mi@hotmail.com>
2023-01-12 20:19:17 -0800

    Inicio de Sesion

---
+++ MiolectricasWeb/Pages/IniciarSesion.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiolectricasWeb.Pages
+{
+    public class IniciarSesionModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
