def824c6173e89563e3bbf450b90133ab9ec8b85
= <=>
2023-01-12 19:38:21 -0800

    1

---
+++ MiolectricasWeb/Pages/Prueba.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace MiolectricasWeb.Pages
+{
+    public class PruebaModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
